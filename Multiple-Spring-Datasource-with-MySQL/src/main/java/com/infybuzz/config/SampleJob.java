9a0e59c935a7c72d2c9a608a24481058eedcd9e4
Satyam Jaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2024-01-08 17:50:42 +0530

    Add files via upload

---
+++ Multiple-Spring-Datasource-with-MySQL/src/main/java/com/infybuzz/config/SampleJob.java
@@ -0,0 +1,193 @@
+package com.infybuzz.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import com.infybuzz.model.StudentCsv;
+import com.infybuzz.model.StudentJdbc;
+import com.infybuzz.model.StudentJson;
+import com.infybuzz.model.StudentXml;
+import com.infybuzz.processor.FirstItemProcessor;
+import com.infybuzz.reader.FirstItemReader;
+import com.infybuzz.writer.FirstItemWriter;
+
+@Configuration
+public class SampleJob {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	private FirstItemReader firstItemReader;
+	
+	@Autowired
+	private FirstItemProcessor firstItemProcessor;
+	
+	@Autowired
+	private FirstItemWriter firstItemWriter;
+	
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource datasource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	@ConfigurationProperties(prefix = "spring.universitydatasource")
+	public DataSource universitydatasource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	public Job chunkJob() {
+		return jobBuilderFactory.get("Chunk Job")
+				.incrementer(new RunIdIncrementer())
+				.start(firstChunkStep())
+				.build();
+	}
+	
+	private Step firstChunkStep() {
+		return stepBuilderFactory.get("First Chunk Step")
+				.<StudentJdbc, StudentJdbc>chunk(3)
+				//.reader(flatFileItemReader(null))
+				//.reader(jsonItemReader(null))
+				//.reader(staxEventItemReader(null))
+				.reader(jdbcCursorItemReader())
+				//.processor(firstItemProcessor)
+				.writer(firstItemWriter)
+				.build();
+	}
+	
+	@StepScope
+	@Bean
+	public FlatFileItemReader<StudentCsv> flatFileItemReader(
+			@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource) {
+		FlatFileItemReader<StudentCsv> flatFileItemReader = 
+				new FlatFileItemReader<StudentCsv>();
+		
+		flatFileItemReader.setResource(fileSystemResource);
+		
+		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() {
+			{
+				setLineTokenizer(new DelimitedLineTokenizer() {
+					{
+						setNames("ID", "First Name", "Last Name", "Email");
+					}
+				});
+				
+				setFieldSetMapper(new BeanWrapperFieldSetMapper<StudentCsv>() {
+					{
+						setTargetType(StudentCsv.class);
+					}
+				});
+				
+			}
+		});
+		
+		/*
+		DefaultLineMapper<StudentCsv> defaultLineMapper = 
+				new DefaultLineMapper<StudentCsv>();
+		
+		DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+		delimitedLineTokenizer.setNames("ID", "First Name", "Last Name", "Email");
+		
+		defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
+		
+		BeanWrapperFieldSetMapper<StudentCsv> fieldSetMapper = 
+				new BeanWrapperFieldSetMapper<StudentCsv>();
+		fieldSetMapper.setTargetType(StudentCsv.class);
+		
+		defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+		
+		flatFileItemReader.setLineMapper(defaultLineMapper);
+		*/
+		
+		flatFileItemReader.setLinesToSkip(1);
+		
+		return flatFileItemReader;
+	}
+
+	@StepScope
+	@Bean
+	public JsonItemReader<StudentJson> jsonItemReader(
+			@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource) {
+		JsonItemReader<StudentJson> jsonItemReader = 
+				new JsonItemReader<StudentJson>();
+		
+		jsonItemReader.setResource(fileSystemResource);
+		jsonItemReader.setJsonObjectReader(
+				new JacksonJsonObjectReader<>(StudentJson.class));
+		
+		jsonItemReader.setMaxItemCount(8);
+		jsonItemReader.setCurrentItemCount(2);
+		
+		return jsonItemReader;
+	}
+	
+	@StepScope
+	@Bean
+	public StaxEventItemReader<StudentXml> staxEventItemReader(
+			@Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource) {
+		StaxEventItemReader<StudentXml> staxEventItemReader = 
+				new StaxEventItemReader<StudentXml>();
+		
+		staxEventItemReader.setResource(fileSystemResource);
+		staxEventItemReader.setFragmentRootElementName("student");
+		staxEventItemReader.setUnmarshaller(new Jaxb2Marshaller() {
+			{
+				setClassesToBeBound(StudentXml.class);
+			}
+		});
+		
+		return staxEventItemReader;
+	}
+	
+	public JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader() {
+		JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader = 
+				new JdbcCursorItemReader<StudentJdbc>();
+		
+		jdbcCursorItemReader.setDataSource(universitydatasource());
+		jdbcCursorItemReader.setSql(
+				"select id, first_name as firstName, last_name as lastName,"
+				+ "email from student");
+		
+		jdbcCursorItemReader.setRowMapper(new BeanPropertyRowMapper<StudentJdbc>() {
+			{
+				setMappedClass(StudentJdbc.class);
+			}
+		});
+		
+		//jdbcCursorItemReader.setCurrentItemCount(2);
+		//jdbcCursorItemReader.setMaxItemCount(8);
+		
+		return jdbcCursorItemReader;
+	}
+}
---
+++ Multiple-Spring-Datasource-with-MySQL/src/main/java/com/infybuzz/writer/FirstItemWriter.java
@@ -0,0 +1,19 @@
+package com.infybuzz.writer;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import com.infybuzz.model.StudentJdbc;
+
+@Component
+public class FirstItemWriter implements ItemWriter<StudentJdbc> {
+
+	@Override
+	public void write(List<? extends StudentJdbc> items) throws Exception {
+		System.out.println("Inside Item Writer");
+		items.stream().forEach(System.out::println);
+	}
+
+}
