5c679b4a9e3999496a6b6f338ffafc0d0ad657cc
Satyam Jaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2024-01-09 19:24:31 +0530

    Update SpringBatchApplication.java

--- Batch_Processing_Concepts_Task/Second-Tasklet-Step_ServiceClass_Application/src/main/java/com/infybuzz1/app/SpringBatchApplication.java
+++ Batch_Processing_Concepts_Task/Second-Tasklet-Step_ServiceClass_Application/src/main/java/com/infybuzz1/app/SpringBatchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@ComponentScan({"com.infybuzz.config","com.infybuzz.service"})
+@ComponentScan({"com.infybuzz1.config","com.infybuzz1.service"})
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
