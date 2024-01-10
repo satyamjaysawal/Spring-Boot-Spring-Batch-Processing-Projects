f1c1352205ad1f2eb89660514e43bd314caf7abf
Satyam Jaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2024-01-10 16:28:43 +0530

    Add files via upload

---
+++ Batch_Processing_Concepts_Task/Stop-Job-with-REST-API/src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -0,0 +1,23 @@
+package com.infybuzz.app;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan({"com.infybuzz.config", "com.infybuzz.service",
+	"com.infybuzz.listener", "com.infybuzz.reader", "com.infybuzz.processor", 
+	"com.infybuzz.writer", "com.infybuzz.controller"})
+@EnableAsync
+@EnableScheduling
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
